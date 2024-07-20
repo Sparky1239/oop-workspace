440cbb7d1f49bb9bfcfc9bd6e13c011a66702df7
root <root@OliverPC>
2024-07-20 22:38:12 +0930

    function 1

---
+++ practical-01/function-1-1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+extern int array_sum(int [], int);
+
+int main() {
+    int array[5] = {4,5,6,7,8};
+    std::cout << "The total is: " << array_sum(array, 5) << std::endl;
+    return 0;
+}
