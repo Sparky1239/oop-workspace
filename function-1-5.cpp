f0363503906738121d32d329cd8ebd22ea31b3f2
root <root@OliverPC>
2024-07-22 16:53:23 +0930

    litttle silly fix

--- function-1-5.cpp
+++
@@ -1,9 +0,0 @@
-int array_sum(int n) {
-    int total = 0; 
-    for (int i = 1; i <= n; i++) { 
-        if (i % 2 == 0){
-            total++; 
-        }
-    }
-    return total; 
-}
--- main-1-5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-
-extern int array_sum(int);
-
-int main() {
-    std::cout << "The total even numbers are: " << array_sum(21) << std::endl;
-    return 0;
-}
