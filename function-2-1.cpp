f6c3fa8464510f0e26a55446f24b438be446c789
root <root@OliverPC>
2024-07-23 18:09:16 +0930

    case for array 1 or less

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,5 +1,8 @@
 int min_element(int array[], int n) {
     int smallest = array[0]; 
+    if (n<=1){
+        return 0;
+    }
     for (int i = 1; i < n; i++) { 
         if (array[i]<smallest) {
             smallest = array[i];
