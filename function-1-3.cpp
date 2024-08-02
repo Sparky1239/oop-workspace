ae4ac54d78eefe9bff26f1e1d0f84c7798223d62
root <root@OliverPC>
2024-08-02 10:45:42 +0930

    fix final i think

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void count_digits(int array[4][4]) {
-    int count[9] = {0};
+    int count[10] = {0};
     for (int i=0; i<4; i++){
         for (int j=0; j<4; j++) {
             if (array[i][j]<10 && array[i][j]>=0){
