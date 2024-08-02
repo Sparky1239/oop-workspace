940aa897fbd7c70b836176fdb703f646ba42f9b0
root <root@OliverPC>
2024-08-02 10:22:11 +0930

    function 3

---
+++ function-1-3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void count_digits(int array[4][4]) {
+    int count[9] = {0};
+    for (int i=0; i<4; i++){
+        for (int j=0; j<4; j++) {
+            if (array[i][j]<10 && array[i][j]>=0){
+                count[array[i][j]]++;
+            }
+        }
+    }
+    for (int i=0; i<=9; i++){
+        cout << i<< ":" << count[i] << ";";
+
+    }
+    cout << "\n";
+}
