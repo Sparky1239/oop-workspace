bf562bae082909bc74b9217c994c6a62ea859570
root <root@OliverPC>
2024-08-02 10:42:32 +0930

    function 3 fix

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -10,7 +10,7 @@ void count_digits(int array[4][4]) {
             }
         }
     }
-    for (int i=0; i<9; i++){
+    for (int i=0; i<10; i++){
         cout << i<< ":" << count[i] << ";";
 
     }
