6fe8c674eb57c438de3baf3da8b8e6f68d8bf4c9
root <root@OliverPC>
2024-08-02 10:38:06 +0930

    function 3 fix

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -5,10 +5,10 @@ extern void count_digits(int array[4][4]);
 
 int main() {
     int array[4][4] = {
-    {1,0,0,0},
-    {0,1,0,0},
-    {0,0,1,0},
-    {0,0,0,1}
+    {1,2,3,4},
+    {5,1,6,7},
+    {8,9,1,10},
+    {11,12,13,14}
     };
     count_digits(array);             
 }
