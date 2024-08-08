ec6b78b7ca3a7213518365229ff57cb37468e46b
Sparky1239 <user@DESKTOP-5V2M9LK.localdomain>
2024-08-08 14:05:58 +0930

    maybe i didnt commit

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -3,9 +3,9 @@ using namespace std;
 extern double* duplicateArray(double*, int);
 
 int main(){
-    double array[3] = {1.1, 3.3, 0.2};
-    double* ptr = duplicateArray(array, 3);
-    for (int i=0; i<3; i++){
+    double array[5] = {1.1, 3.3, 0.2, 6.3, 3.9};
+    double* ptr = duplicateArray(array, 5);
+    for (int i=0; i<5; i++){
         cout << ptr[i] << "\n";
     }
 }
