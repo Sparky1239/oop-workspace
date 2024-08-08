c09fceafc36304967be70ffda81c65d9b11040f9
Sparky1239 <user@DESKTOP-5V2M9LK.localdomain>
2024-08-08 13:42:27 +0930

    if this works its dumb

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-
+using namespace std;
 double* duplicateArray(double* array, int size){
     double copy[size];
     for (int i=0; i<size; i++){
         copy[i] = array[i];
     }
-    return copy;
+    for (int i=0; i<size; i++){
+        cout << copy[i] << "\n";
+    }
 }
