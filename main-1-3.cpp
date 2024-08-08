195c5e0b1037a970ebf593c13ed39e7ecc2c900e
Sparky1239 <user@DESKTOP-5V2M9LK.localdomain>
2024-08-08 14:02:01 +0930

    fix *billion

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
-extern double duplicateArray(double*, int);
+extern double* duplicateArray(double*, int);
 
 int main(){
     double array[3] = {1.1, 3.3, 0.2};
-    duplicateArray(array, 3);
+    double* ptr = duplicateArray(array, 3);
+    for (int i=0; i<3; i++){
+        cout << ptr[i] << "\n";
+    }
 }
