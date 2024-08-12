9aefe5e21d375ad2c022f29ad910b9472c673467
root <root@OliverPC>
2024-08-12 10:50:06 +0930

    weird

--- function-3-2.cpp
+++ function-3-2.cpp
@@ -11,6 +11,14 @@ int *readNumbers(){
     return numbers;
 }
 
+bool equalsArray(int *numbers1,int *numbers2,int length){
+    bool same = 1;
+    for (int i=0; i<length; i++){
+        if (numbers1[i] != numbers2[i])
+        same= 0;
+    }
+    return same;
+}
 
 int *reverseArray(int *numbers1 ,int length){
     int* reverse = new int[length];
