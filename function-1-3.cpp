d2a36ffd3e8313f91efd56f5909125b5c31839c3
Sparky1239 <user@DESKTOP-5V2M9LK.localdomain>
2024-08-15 12:47:18 +0930

    puttting function 2 in function 3

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -12,3 +12,19 @@ PersonList deepCopyPersonList(PersonList pl) {
 
     return newList;
 }
+
+
+PersonList createPersonList (int n){
+    PersonList list;
+    list.numPeople= n;
+    list.people = new Person[n];
+
+
+
+
+    for(int i = 0; i < n; i++){
+        list.people[i].name = "Jane Doe";
+        list.people[i].age = 1;
+    }
+    return list;
+}
