be98dad10c798f83ab4f7296bd037c91c365c32f
root <root@OliverPC>
2024-08-23 10:21:11 +0930

    little fix

--- Musician.cpp
+++ Musician.cpp
@@ -1,9 +1,11 @@
 #include "Musician.h"
 
-Musician::Musician() : (string instrument,)
+Musician::Musician() : instrument("null"), experience(0) {}
 
-Musician::Musician(std::string instrument, int experience) ...
+Musician::Musician(std::string instrument, int experience) : instrument("instrument"), experience(experience) {}
 
-std::string Musician::get_instrument()...
+std::string Musician::get_instrument() : {
+    return instrument;
+}
 
 int Musician::get_experience() ...
