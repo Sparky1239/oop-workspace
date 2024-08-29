78694e13642a000c391dbcd8279ba4cce943064d
Sparky1239 <user@DESKTOP-5V2M9LK.localdomain>
2024-08-29 14:14:44 +0930

    getting there

---
+++ main-1-2.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+#include "StockItem.h"
+#include "Store.h"
+using namespace std;
+
+int main(){
+StockItem apple = StockItem(1, "apple");
+StockItem orange = StockItem(2, "orange");
+StockItem banana = StockItem(3, "banana");
+StockItem peach = StockItem(4, "peach");
+
+
+Store storeA = Store(4);
+cout << storeA.add_Stock(apple) << storeA.add_Stock(orange) << storeA.add_Stock(banana) << storeA.add_Stock(peach)<< "\n";
+
+cout << storeA.get_Total_Stock_Count() << "\n";
+
+storeA.~Store();
+
+
+
+
+return 0;
+}
