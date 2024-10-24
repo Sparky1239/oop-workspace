0e6d78928665b0e6d99d539376d691f49f1f4c64
Sparky1239 <user@DESKTOP-5V2M9LK.localdomain>
2024-10-24 13:51:52 +1030

    functionality of question 1

--- main-1.cpp
+++ main-1.cpp
@@ -2,5 +2,12 @@
 #include "Helper.hpp"
 
 int main(){
+    Helper Helper;
+    GridItem Block1 = GridItem(1, 1, 1, 1);
+    GridItem Block2 = GridItem(5, 5, 1, 1);
+    
+    std::cout<<"\nblock position 1: " <<Block1.getCoordinates().first<< " "<<Block1.getCoordinates().second;
+    std::cout<<"\nblock position 2: " <<Block2.getCoordinates().first<< " "<<Block2.getCoordinates().second;
 
+    std::cout<<"\ndistance between the blocks is: " << Helper.euclideanDistance(Block1.getCoordinates(), Block2.getCoordinates());
 }
