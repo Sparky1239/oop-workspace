e6f44705c39d2c9a299f2a9331f0a3d0d808344e
Sparky1239 <user@DESKTOP-5V2M9LK.localdomain>
2024-10-24 14:06:26 +1030

    stupid thing will compile on my computer but not on gradescope

--- Helper.hpp
+++ Helper.hpp
@@ -7,7 +7,7 @@
 class Helper {
 public:
     //function to calculate the euclidean distance betweeen two coordinates.
-    double euclideanDistance(std::pair<int, int> item1, std::pair<int, int> item2) {
+    static double euclideanDistance(std::pair<int, int> item1, std::pair<int, int> item2) {
         int x1 = item1.first;           int y1 = item1.second;
         int x2 = item2.first;           int y2 = item2.second;
         return sqrt(pow(x2-x1, 2)+ pow(y2-y1, 2));
